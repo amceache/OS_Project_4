ed8854fbed4fb3b7e50222a39f6dc74c0521391f
Kaitlyn Ingram <kingram1@nd.edu>
2018-04-03 22:51:23 -0400

    added .h file

--- hashtable.h
+++ hashtable.h
@@ -1,30 +1,16 @@
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
 
+#include <string>
+
+using namespace std;
+
 /* STRUCTS ------------------------------------------------------------------ */
 
 struct packet
 {
-    size_t hash; // Hash function of the data created by hash_fn
-    char * packet_data;
+    int start; // Hash function of the data created by hash_fn
+    string s;
 };
-
-typedef struct packet entry_t;
-
-struct table
-{
-    int size;
-    struct packet ** table;
-};
-
-typedef struct table hashtable_t;
-
-/* PROTOTYPES --------------------------------------------------------------- */
-
-hashtable_t * createTable (int size);
-int generateHash (hashtable_t * hashtable, char * packet_header);
-entry_t * newNode (char * packet_header, char * packet_data);
-void insert (hashtable_t * hashtable, char * packet_header, char * packet_data);
-char * lookup (hashtable_t * hashtable, char * packet_header);
 
 #endif
